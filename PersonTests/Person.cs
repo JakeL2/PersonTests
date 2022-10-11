c6f7a9f5b3b5e9ca8bb0e08419dcc025c3496f90
John <johnDoe@outlook.com>
2022-10-11 19:50:34 -0400

    Added a little bit but not fully functional

--- PersonTests/Person.cs
+++ PersonTests/Person.cs
@@ -3,16 +3,19 @@
     /// <summary>
     /// Represents one person saving their name.
     /// </summary>
-    public class Person
+    public class Person : IDisplayable
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Person"/> class.
         /// Constructor for a person optionally adding their name.
         /// </summary>
         /// <param name="name">Person's name, empty by default.</param>
-        public Person(string name = "")
+        public Person(string name = "", int size = 0, int reset = 0, int display = 0 )
         {
             this.Name = name.Trim();
+            this.Sizes = size;
+            this.Resets = reset;
+            this.Displays = display;
         }
 
         /// <summary>
@@ -28,5 +31,23 @@
         {
             return this.Name;
         }
+
+        
+        public int Sizes { get; set; }
+
+        public int Resets { get; set; }
+
+        public int Displays { get; set; }
+
+        int IDisplayable.Size() { return this.Sizes; }
+
+        int IDisplayable.Reset() { return this.Resets; }
+
+        int IDisplayable.Display() { return this.Displays; }
+
+        
+
+
+
     }
 }
